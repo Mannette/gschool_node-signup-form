bed11f45ec927dcec8a717aa637c7cc60c6f6fd7
ethan <ethanmannette@gmail.com>
2015-08-25 19:48:43 -0600

    Added form. Redirects to success page, does not validate email

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Node Signup' });
+});
+
+router.post('/submit', function (req, res, next) {
+  if (req.body.email !== '') {
+    res.redirect('/success');
+  }
+
+  res.send(req.body.email);
+});
+
+router.get('/success', function (req, res, next) {
+  res.send('Success!')
 });
 
 module.exports = router;
